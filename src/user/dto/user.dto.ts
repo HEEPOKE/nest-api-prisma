880f63c3545de0d1dae4928e085c3392d1bd88f6
HEEPOKE <Damon1fx@gmail.com>
2023-05-18 17:25:16 +0700

    :money_with_wings: set default input

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/mapped-types';
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 class CreateUserDto {
   @IsEmail()
+  @ApiProperty({ example: 'example@gmail.com' })
   email: string;
 
   @IsString()
+  @ApiProperty({ example: 'heePoke' })
   username: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ example: '12345678' })
   password: string;
 
   @IsString()
+  @ApiProperty({ example: '0000000000' })
   tel: string;
 }
 
